85acc035feaf477bcfdc5d73e523436021c61d4e
s6135410012 <s6135410012@sau.ac.th>
2021-10-27 15:49:36 +0700

    Code succeed

--- android/app/src/main/kotlin/com/sausoft/moneyshare/MainActivity.kt
+++ android/app/src/main/kotlin/com/sausoft/moneyshare/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_app_moneyshare
+package com.sausoft.moneyshare
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/views/home_ui.dart
+++ lib/views/home_ui.dart
@@ -11,10 +11,57 @@ class HomeUI extends StatefulWidget {
 
 class _HomeUIState extends State<HomeUI> {
   bool checkTip = false;
-    //สร้างตัวควบคุม TextField
+  //สร้างตัวควบคุม TextField
   TextEditingController txMoney = TextEditingController();
   TextEditingController txPerson = TextEditingController();
   TextEditingController txTip = TextEditingController();
+
+  // เมธอด -> โค้ดแสดง Dialog เตือน โดยจะรับข้อความมาแสดงที่ Dialog
+  showWrarningDialog(contex, msg) {
+    //เรียกใช้งานฟังก์ชัน showDialog
+    showDialog(
+      context: contex,
+      builder: (contex) {
+        return AlertDialog(
+          title: Container(
+            color: Colors.deepPurple,
+            padding: EdgeInsets.only(
+              top: 10.0,
+              bottom: 10.0,
+            ),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                'คำเตือน',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          content: Text(msg),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(contex);
+                  },
+                  child: Text(
+                    'ตกลง',
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.deepPurple,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -80,7 +127,7 @@ class _HomeUIState extends State<HomeUI> {
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(
-                  horizontal: 40.0, 
+                  horizontal: 40.0,
                 ),
                 child: TextField(
                   controller: txPerson,
@@ -109,16 +156,16 @@ class _HomeUIState extends State<HomeUI> {
               ),
               SizedBox(
                 height: 20.0,
-              ),              
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Checkbox(
                     onChanged: (data) {
                       setState(() {
-                        if( data != null){
+                        if (data != null) {
                           checkTip = data;
-                          if(data == false){
+                          if (data == false) {
                             txTip.text = '';
                           }
                         }
@@ -127,9 +174,9 @@ class _HomeUIState extends State<HomeUI> {
                     value: checkTip,
                     side: BorderSide(
                       color: Colors.deepPurple,
-                    ), 
-                    activeColor: Colors.deepPurple,        
-                    checkColor: Colors.red,       
+                    ),
+                    activeColor: Colors.deepPurple,
+                    checkColor: Colors.red,
                   ),
                   Text(
                     'ทิปให้พนักงานเสริฟ',
@@ -165,9 +212,9 @@ class _HomeUIState extends State<HomeUI> {
                     ),
                     hintText: 'ป้อนจำนวนเงินทิป (บาท)',
                     hintStyle: TextStyle(
-                      color: Colors.grey[400],                   
-                    ),                 
-                  ),                  
+                      color: Colors.grey[400],
+                    ),
+                  ),
                   keyboardType: TextInputType.number,
                 ),
               ),
@@ -176,33 +223,67 @@ class _HomeUIState extends State<HomeUI> {
               ),
               ElevatedButton(
                 onPressed: () {
+                  if (txMoney.text.length == 0) {
+                    showWrarningDialog(context, 'ป้อนจำนวนเงิน');
+                  } else if (txPerson.text.length == 0) {
+                    showWrarningDialog(context, 'ป้อนจำนวนคน');
+                  } else {
+                    if (checkTip == true) {
+                      if (txTip.text.length == 0) {
+                        showWrarningDialog(context, 'ป้อนจำนวนเงิน(ทิป)');
+                        return;
+                      }
+                    }
+                    //กรณีป้อนครบเรียบร้อยก็คำนวณ
+                    double money = 0;
+                    int person = 0;
+                    double tip = 0;
+                    double moneyshare = 0;
+                    money = double.parse(txMoney.text);
+                    person = int.parse(txPerson.text);
+                    tip = checkTip == true ? double.parse(txTip.text) : 0;
+                    moneyshare = (money + tip) / person;
+
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return MoneyShareUI(
+                        money: money,
+                        person: person,
+                        tip: tip,
+                        moneyshare: moneyshare,
+                      );
+                    }));
+                  }
+
                   //ตรวจสอบป้อนหรือยังในสิ่งที่ต้องป้อน ถ้ายังแสดง dialog เตือน
+                  // showWrarningDialog(
+                  //   context, 'ทดสอบ......',
+                  //  );
+
                   //กรณีป้อนครบก็คำนวณ
                   //เมือคำนวณเรีบร้อยแล้วให้ส่งข้อมูลไปแสดงผลที่หน้า moneyshareUI
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context){
-                        return MoneyShareUI();
-                      },
-                    ),
-                  );
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //     builder: (context){
+                  //       return MoneyShareUI();
+                  //     },
+                  //   ),
+                  // );
                 },
                 child: Text(
                   'คำนวณ',
                 ),
                 style: ElevatedButton.styleFrom(
-                  fixedSize: Size(
-                    MediaQuery.of(context).size.width - 80.0,
-                    55.0
+                  fixedSize:
+                      Size(MediaQuery.of(context).size.width - 80.0, 55.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      55.0,
                     ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                        55.0,
-                      ),
-                    ),
-                    primary: Colors.deepPurple,
-                    elevation: 10.0, //เงา
+                  ),
+                  primary: Colors.deepPurple,
+                  elevation: 10.0, //เงา
                 ),
               ),
               SizedBox(
@@ -211,10 +292,10 @@ class _HomeUIState extends State<HomeUI> {
               ElevatedButton.icon(
                 onPressed: () {
                   setState(() {
-                  txMoney.text = '';
-                  txPerson.text = '';
-                  txTip.text = '';
-                  checkTip = false;
+                    txMoney.text = '';
+                    txPerson.text = '';
+                    txTip.text = '';
+                    checkTip = false;
                   });
                 },
                 icon: Icon(
@@ -224,17 +305,15 @@ class _HomeUIState extends State<HomeUI> {
                   'ยกเลิก',
                 ),
                 style: ElevatedButton.styleFrom(
-                  fixedSize: Size(
-                    MediaQuery.of(context).size.width - 80.0,
-                    55.0
+                  fixedSize:
+                      Size(MediaQuery.of(context).size.width - 80.0, 55.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      55.0,
                     ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(
-                        55.0,
-                      ),
-                    ),
-                    primary: Colors.red,
-                    elevation: 10.0,
+                  ),
+                  primary: Colors.red,
+                  elevation: 10.0,
                 ),
               ),
               SizedBox(
@@ -246,10 +325,10 @@ class _HomeUIState extends State<HomeUI> {
                   fontWeight: FontWeight.bold,
                   color: Color(0xFF454545),
                 ),
-              ),              
+              ),
               SizedBox(
                 height: 50.0,
-              ),              
+              ),
             ],
           ),
         ),
